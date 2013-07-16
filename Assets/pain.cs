973b75cefbb010d369fbac9a43c7c9ee94e5752f
stellak <gk1016@nyu.edu>
2013-07-16 13:09:41 -0400

    update

---
+++ Assets/pain.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class pain : MonoBehaviour {
+	
+	int myInt = 5;
+			
+	// Use this for initialization
+	void Start () 	
+	{
+		myInt=MultiplyByTwo(myInt);
+		Debug.Log (myInt);
+	}
+	
+	int MultiplyByTwo(int number)
+	{
+		int ret;
+		ret = number*2;
+		return ret;
+	}
+		
+	// Update is called once per frame
+	void Update () 
+		
+		{
+	
+	}
+}
